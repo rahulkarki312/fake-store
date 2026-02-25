a2fbf2f8a3e73d317114e0b0557697c8aca1589e
Rahul <mtal3584@gmail.com>
2026-02-25 13:53:54 +0545

    fetch functions updated-2

--- app/(main)/products/page.tsx
+++ app/(main)/products/page.tsx
@@ -2,6 +2,7 @@ import { getAllProducts, getAllCategories } from "@/lib/api/products";
 import ProductsClient from "@/components/product/ProductsClient";
 import SortControls from "@/components/product/SortControls";
 import { Metadata } from "next";
+import { Product } from "@/lib/api/types";
 
 interface ProductsPageProps {
   searchParams: Promise<{
@@ -9,14 +10,12 @@ interface ProductsPageProps {
   }>;
 }
 
-
-
 export async function generateMetadata(): Promise<Metadata> {
   const products = await getAllProducts();
   const categories = await getAllCategories();
 
   const description = `Browse all products from FakeStore across categories: ${categories.join(
-    ", "
+    ", ",
   )}, with prices and filters.`;
 
   return {
@@ -38,14 +37,20 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-
 export default async function ProductsPage({
   searchParams,
 }: ProductsPageProps) {
   const { sort } = await searchParams;
 
-  const products = await getAllProducts(sort);
-  const categories = await getAllCategories();
+  let products: Product[] = [];
+  let categories: string[] = [];
+  try {
+    products = await getAllProducts(sort);
+    categories = await getAllCategories();
+  } catch (error) {
+    console.error("Products page error:", error);
+  }
+
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
 
   // JSON-LD structured data for collection
@@ -55,7 +60,8 @@ export default async function ProductsPage({
     name: "FakeStore Products",
     description:
       "Browse all products from FakeStore with categories, prices, and filters.",
-    hasPart:  products.slice(0, 5).map((p) => ({  // including sample products (only 5 for now) in collection
+    hasPart: products.slice(0, 5).map((p) => ({
+      // including sample products (only 5 for now) in collection
       "@type": "Product",
       name: p.title,
       image: [p.image],
@@ -76,7 +82,6 @@ export default async function ProductsPage({
     })),
   };
 
-
   return (
     <main className="max-w-7xl mx-auto px-4 md:px-14 py-8">
       <script
@@ -85,13 +90,7 @@ export default async function ProductsPage({
       />
       <h1 className="text-2xl font-semibold mb-6">Products</h1>
 
-
-       <ProductsClient
-        products={products}
-        categories={categories}
-        sort={sort}
-      />
-
+      <ProductsClient products={products} categories={categories} sort={sort} />
     </main>
   );
 }
