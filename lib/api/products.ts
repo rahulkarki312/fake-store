b9eda89023ada7e500a983a5e3ac8ca46f7b3156
Rahul <mtal3584@gmail.com>
2026-02-25 16:58:09 +0545

    undo changes

--- lib/api/products.ts
+++ lib/api/products.ts
@@ -1,9 +1,8 @@
 
-import { fetcher } from "../fetcher";
-import { Product } from "../types";
+import { fetcher } from "./fetcher";
+import { Product } from "./types";
 
 const BASE_URL =  process.env.NEXT_PUBLIC_API_URL;
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
 
 
 export async function getAllProducts(
@@ -11,7 +10,7 @@ export async function getAllProducts(
 ): Promise<Product[]> {
   const query = sort ? `?sort=${sort}` : "";
 
-  return fetcher<Product[]>(`${siteUrl}/api/products${query}`, {
+  return fetcher<Product[]>(`${BASE_URL}/products${query}`, {
     
     cache: "no-store"
 
@@ -21,13 +20,13 @@ export async function getAllProducts(
 export async function getProductById(
   id: string
 ): Promise<Product> {
-  return fetcher<Product>(`${siteUrl}/api/products/${id}`, {
+  return fetcher<Product>(`${BASE_URL}/products/${id}`, {
     cache: "no-store"
   });
 }
 
 export async function getAllCategories(): Promise<string[]> {
-  return fetcher<string[]>(`${siteUrl}/api/products/categories`, {
+  return fetcher<string[]>(`${BASE_URL}/products/categories`, {
     cache: "no-store"
   });
 }
